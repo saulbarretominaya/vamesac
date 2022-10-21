728aa9e5b1df131084685125b02fc824bae0008e
Saul Barreto Minaya <saulbarretominaya@gmail.com>
2022-10-21 18:28:46 -0500

    agregando un comentario

--- application/views/inicio/V_index.php
+++ application/views/inicio/V_index.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+<!-- Hola brandex -->
 
 <head>
     <meta charset="utf-8">
@@ -10,7 +11,7 @@
     <link rel="stylesheet" href="<?php echo base_url(); ?>plantilla/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-     <!-- No se habilita el bootrap5 porque tiene conflictos con bundle.min.js -->
+    <!-- No se habilita el bootrap5 porque tiene conflictos con bundle.min.js -->
     <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
     <script src="<?php echo base_url(); ?>plantilla/plugins/jquery/jquery.min.js"></script>
     <script src="<?php echo base_url(); ?>plantilla/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
